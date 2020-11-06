d4aeb0bff7b1a2a8518ec97f198d08e05eb211ab
aarora16 <a.99.anshul@gmail.com>
2020-11-06 10:24:19 -0600

    user changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :listings
   has_many :cars, through: :listings
+
+  validates :name, :age, :type_of_car, presence: true
+  validates :name, length: { minimum: 2, maximum: 12 }
+  validates :age, numericality: { greater_than: 0, less_than: 99 }
+  
 end
