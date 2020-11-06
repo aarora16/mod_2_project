3e7f2fd805834ebfd72258ab9c6a0fc2aa8c71af
aarora16 <a.99.anshul@gmail.com>
2020-11-06 10:07:18 -0600

    basic code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   private
