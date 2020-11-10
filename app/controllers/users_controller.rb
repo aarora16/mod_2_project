db6b6fea5924ae0440614f5d0c48b30e9b863c34
aarora16 <a.99.anshul@gmail.com>
2020-11-10 09:44:21 -0600

    users update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,26 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
 
   def create
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user)
     else
-      render "new"
+      render :new
     end
   end
 
