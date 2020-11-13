820341469ffb94b5c3ea1c958bec9de50317303f
aarora16 <a.99.anshul@gmail.com>
2020-11-13 14:04:11 -0600

    push

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find(params[:id])
-    
+    session[:username] = params[:username]
+  end
+
+  def destroy
+    session.delete :username
   end
 end
